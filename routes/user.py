6ded2f968e9e86274536f77b804cc237befb64c4
mrvasil <alexeyka.v@yandex.ru>
2024-10-22 13:25:52 +0300

    add user_type and fix password

--- routes/user.py
+++ routes/user.py
@@ -10,9 +10,10 @@ def index():
 @user_bp.route("/add_user", methods=["POST"])
 def add_user():
     real_name = request.json.get('real_name')
+    user_type = request.json.get('user_type')
     if not real_name:
         return jsonify({"error": "Real name is required"}), 400
-    user, password = UserManager.add_user(real_name)
+    user, password = UserManager.add_user(real_name, user_type)
     return jsonify({"success": True, "username": str(user.login), "password": password}), 201
 
 @user_bp.route("/get_user", methods=["GET"])
@@ -59,13 +60,14 @@ def add_users():
     added_users = []
     for user_info in users_data:
         real_name = user_info.get('real_name')
+        user_type = user_info.get('user_type')
         if not real_name:
             return jsonify({"error": f"Real name is required for all users. Missing for: {user_info}"}), 400
-        user = UserManager.add_user(real_name)
+        user, password = UserManager.add_user(real_name, user_type)
         added_users.append({
             "username": str(user.login),
-            "password": user.password,
+            "password": password,
             "real_name": real_name
         })
-    
+        
     return jsonify({"success": True, "added_users": added_users}), 201
