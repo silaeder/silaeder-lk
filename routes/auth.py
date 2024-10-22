b677be1b75ef2ca31a54468ce51832259eea73b0
ilyastar9999 <ilya.starchak@yandex.com>
2024-10-22 12:49:32 +0300

    auth update

--- routes/auth.py
+++ routes/auth.py
@@ -1,58 +1,16 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, jsonify
 import jwt
 import os
+import hashlib
+from database import user
 
 auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route("/login", methods=["GET", "POST"])
+@auth_bp.route("/login", methods=["GET"])
 def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    else:
-        form = request.form
-        for i in form.keys():
-            if form[i] == "":
-                return "Empty field: " + i
-        if 1==1:
-            response = redirect(url_for("portfolio.index"))
-            response.set_cookie("token", jwt.encode({"user": form["username"]}, os.getenv("JWT_SECRET"), algorithm="HS256"))
-            return response
-
-@auth_bp.route("/logout")
-def logout():
-    return redirect(url_for("auth.login"))
-
-@auth_bp.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "GET":
-        return render_template("register.html")
-    else:
-        form = request.form
-        for i in form.keys():
-            if form[i] == "":
-                return "Empty field: " + i
-        if 1==1:
-            response = redirect(url_for("portfolio.index"))
-            response.set_cookie("token", jwt.encode({"user": form["username"]}, os.getenv("JWT_SECRET"), algorithm="HS256"))
-            return response
-
-@auth_bp.route("/change-password", methods=["GET", "POST"])
-def change_password():
-    if request.method == "GET":
-        if request.cookies.get("token"):
-            try:
-                if jwt.decode(request.cookies.get("token"), os.getenv("JWT_SECRET"), algorithms=["HS256"]):
-                    return render_template("change-password.html")
-                else:
-                    return redirect(url_for("auth.login"))
-            except:
-                return redirect(url_for("auth.login"))
-        else:
-            return redirect(url_for("auth.login"))
-    else:
-        form = request.form
-        for i in form.keys():
-            if form[i] == "":
-                return "Empty field: " + i
-        if 1==1:    
-            return redirect(url_for("portfolio.index"))
+    login = request.args.get("login")
+    password = hashlib.md5(request.args.get("password").encode()).hexdigest()
+    if user.User.check_credentials(login, password):
+        token = jwt.encode({"login": login}, os.getenv("SECRET_KEY"), algorithm="HS256")
+        return jsonify({"success": True, "token": token})
+    return jsonify({"success": False})
