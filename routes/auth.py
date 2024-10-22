4616a61113631794fbbda3f15ad0c0fb01808634
ilyastar9999 <ilya.starchak@yandex.com>
2024-10-22 12:53:04 +0300

    decorator added

--- routes/auth.py
+++ routes/auth.py
@@ -3,6 +3,7 @@ import jwt
 import os
 import hashlib
 from database import user
+from functools import wraps
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -11,6 +12,19 @@ def login():
     login = request.args.get("login")
     password = hashlib.md5(request.args.get("password").encode()).hexdigest()
     if user.User.check_credentials(login, password):
-        token = jwt.encode({"login": login}, os.getenv("SECRET_KEY"), algorithm="HS256")
+        token = jwt.encode({"login": login}, os.getenv("JWT_SECRET"), algorithm="HS256")
         return jsonify({"success": True, "token": token})
     return jsonify({"success": False})
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=["HS256"])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(*args, **kwargs)
+    return decorated
