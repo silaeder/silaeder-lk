ccc11fd9d35b1e4b71739f53536414a7b4614b1a
ilyastar9999 <ilya.starchak@yandex.com>
2024-10-23 11:30:31 +0300

    proj update

--- database/projects.py
+++ database/projects.py
@@ -1,23 +1,84 @@
 from flask_sqlalchemy import SQLAlchemy
 
 from . import db
-
+from .user import UserManager
 class Project(db.Model):
     __tablename__ = 'projects'
     project_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    short_description = db.Column(db.String(200), nullable=False)
     video_link = db.Column(db.String(200))
-    presentation_path = db.Column(db.String(300))
+    presentation_path = db.Column(db.String(100))
     teacher = db.Column(db.String(100), nullable=False) #научный руководитель (его login)
-    team = db.Column(db.String(100), nullable=False) #список участников (логины)
     status = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f"<Project {self.title}>"
     
+class ProjectUser(db.Model):
+    __tablename__ = 'project_users'
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'), nullable=False)
+    login = db.Column(db.String(100), db.ForeignKey('users.login'), nullable=False)
+
+    def __repr__(self):
+        return f"<ProjectUser {self.project_id} {self.user_id}>"
+
+class ProjectManager:
+    @staticmethod
+    def create_project(project):
+        db.session.add(project)
+        db.session.commit()
+        return project
+    
+    @staticmethod
     def get_project_by_id(project_id):
         return Project.query.filter_by(project_id=project_id).first()
     
-    def get_projects_by_user_id(user_id):
-        return Project.query.filter_by(user_id=user_id).all()
+    @staticmethod
+    def get_projects_by_user_id(login):
+        projects = ProjectUser.query.filter_by(login=login).all()
+        return [ProjectManager.get_project_by_id(project.project_id) for project in projects] if projects else []
+    
+    @staticmethod
+    def add_user_to_project(project_id, login):
+        if not ProjectManager.get_project_by_id(project_id):
+            return -1
+        if not UserManager.get_user_by_login(login):
+            return -2
+        project_user = ProjectUser(project_id=project_id, login=login)
+        db.session.add(project_user)
+        db.session.commit()
+        return project_user
+    
+    @staticmethod
+    def remove_user_from_project(project_id, login):
+        if not ProjectManager.get_project_by_id(project_id):
+            return -1
+        if not UserManager.get_user_by_login(login):
+            return -2
+        project_user = ProjectUser.query.filter_by(project_id=project_id, login=login).first()
+        if project_user:
+            db.session.delete(project_user)
+            db.session.commit()
+            return True
+        return False
+    
+    @staticmethod
+    def get_users_by_project_id(project_id):
+        if not ProjectManager.get_project_by_id(project_id):
+            return -1
+        users = ProjectUser.query.filter_by(project_id=project_id).all()
+        return [UserManager.get_user_by_email(user.login) for user in users] if users else []
+    
+    @staticmethod
+    def delete_project(project_id):
+        project = Project.query.filter_by(project_id=project_id).first()
+        if project:
+            db.session.delete(project)
+            db.session.commit()
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"<ProjectManager {self.project_id} {self.user_id}>"
--- routes/projects.py
+++ routes/projects.py
@@ -1,12 +1,104 @@
 from flask import Blueprint, request, jsonify, render_template
 from database.projects import Project
 from routes.auth import auth_required
+from database import db
+from database.projects import ProjectManager
+
+PROJECT_FIELDS = ["title", "description", "teacher", "team", "status", "video_link", "presentation_path"]
+PROJECT_REQUIRED_FIELDS = ["title", "description", "teacher", "team", "status"]
 
 projects_bp = Blueprint("projects", __name__)
 
-@projects_bp.route("/get_user", methods=["GET"])
+@projects_bp.route("/")
+def index():
+    return "Projects"
+
+@projects_bp.route("/get_project_members", methods=["GET"])
 @auth_required
-def get_user_projects():
-    user_id = request.args.get("user_id")
-    projects = Project.get_projects_by_user_id(user_id)
+def get_project_members():
+    if not request.args.get("project_id"):
+        return jsonify({"message": "Project ID is required"}), 400
+    project_id = request.args.get("project_id")
+    projects = ProjectManager.get_users_by_project_id(project_id)
     return jsonify({"projects": [project.to_dict() for project in projects]})
+
+@projects_bp.route("/get_project", methods=["GET"])
+@auth_required
+def get_project():
+    if not request.args.get("project_id"):
+        return jsonify({"message": "Project ID is required"}), 400
+    project_id = request.args.get("project_id")
+    project = ProjectManager.get_project_by_id(project_id)
+    return jsonify(project.to_dict())
+
+@projects_bp.route("/get_projects_by_user", methods=["GET"])
+@auth_required
+def get_projects_by_user():
+    if not request.args.get("login"):
+        return jsonify({"message": "Login is required"}), 400
+    login = request.args.get("login")
+    projects = ProjectManager.get_projects_by_user_login(login)
+    return jsonify([project.to_dict() for project in projects])
+
+@projects_bp.route("/create", methods=["POST"])
+@auth_required
+def create_project():
+    for i in PROJECT_REQUIRED_FIELDS:
+        if i not in request.json:
+            return jsonify({"message": f"Field {i} is required"}), 400
+    project = Project()
+    for i in PROJECT_FIELDS:
+        if i in request.json:
+            setattr(project, i, request.json[i])
+    db.session.add(project)
+    db.session.commit()
+    return jsonify({"message": "Project created"}), 201
+
+@projects_bp.route("/update/<int:project_id>", methods=["POST"])
+@auth_required
+def update_project(project_id):
+    project = ProjectManager.get_project_by_id(project_id)
+    if not project:
+        return jsonify({"message": "Project not found"}), 404
+    for i in PROJECT_FIELDS:
+        if i == "team":
+            if i in request.json:
+                for login in request.json[i]:
+                    st = ProjectManager.add_user_to_project(project_id, login)
+                    if st == -2:
+                        return jsonify({"message": f"User {login} not found"}), 404
+        else:
+            if i in request.json:
+                setattr(project, i, request.json[i])
+    db.session.commit()
+    return jsonify({"message": "Project updated"}), 200
+
+@projects_bp.route("/add_user/<int:project_id>", methods=["POST"])
+@auth_required
+def add_user_to_project(project_id):
+    login = request.json.get("login")
+    st = ProjectManager.add_user_to_project(project_id, login)
+    if st == -1:
+        return jsonify({"message": f"Project {project_id}` not found"}), 404
+    elif st == -2:
+        return jsonify({"message": f"User {login} not found"}), 404
+    return jsonify({"message": "User added to project"}), 200
+
+@projects_bp.route("/remove_user/<int:project_id>", methods=["POST"])
+@auth_required
+def remove_user_from_project(project_id):
+    login = request.json.get("login")
+    st = ProjectManager.remove_user_from_project(project_id, login)
+    if st == -1:
+        return jsonify({"message": f"Project {project_id}` not found"}), 404
+    elif st == -2:
+        return jsonify({"message": f"User {login} not found"}), 404
+    return jsonify({"message": "User removed from project"}), 200
+
+@projects_bp.route("/delete/<int:project_id>", methods=["POST"])
+@auth_required
+def delete_project(project_id):
+    if ProjectManager.delete_project(project_id):
+        return jsonify({"message": "Project deleted"}), 200
+    else:
+        return jsonify({"message": "Project not found"}), 404
